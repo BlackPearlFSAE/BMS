b2bdbb8893e25755c9f28d61c644c7e35f98bea7
nat-chin <138374958+nat-chin@users.noreply.github.com>
2024-10-29 17:00:11 +0700

    Just put code

---
+++ bmsCAN.cpp
@@ -0,0 +1,184 @@
+//  MUST CHANGE CAN LIB to fully support Extended Frame Transmission & Reception like 
+//
+
+/* BMS Master unit Send And Receive to and from OBC*/
+
+#include <Arduino.h>
+#include <ArduinoSTL.h> // C/C++ standard libs comeback
+#include <SPI.h>
+#include <mcp2515.h>
+
+// Byte conversion , bit shifting custom functions
+#include <util.h>
+
+can_frame bmssent;
+can_frame obcreceived;
+MCP2515 mcp2515(10,MCP_16MHZ);
+#define SDCPIN 13 // actual test will use pin4
+
+void setup() {
+  
+  digitalWrite(SDCPIN,HIGH);
+  Serial.begin(115200);
+  /* Set up BMS CAN frame*/
+    bmssent.can_id  = 0x1806E5F4 | CAN_EFF_FLAG;
+    bmssent.can_dlc = 8;
+    // Conserved Byte
+    bmssent.data[5] = 0x00;
+    bmssent.data[6] = 0x00;
+    bmssent.data[7] = 0x00;
+  
+  /* MCP2515 setup */
+  mcp2515.reset(); // Reset SPI 
+  mcp2515.setBitrate(CAN_250KBPS);
+  mcp2515.setNormalMode();
+  
+  Serial.println("------- BMS ----------");
+  // Serial.println("ID  DLC   DATA");
+}
+
+unsigned long last_time = 0;
+void loop() {
+  
+  // /* BMS Native Feature*/
+    //   float cellvolt = 5;
+    //   float dischgAmp = 5;
+    //   // Function to read individual Cell voltage once
+    //   Serial.println();
+    //   Serial.print("Cell Voltage: "); Serial.println(cellvolt,DEC);
+
+
+
+  /* Minimum Voltage of 2 Module : 3.1*20 = 62V , Nominal Volage 3.6*20 = 72V
+  Maximum allowable Voltage for 2 module : 83V => 830 => 0x 03 3E
+  Maximum allowable current for 2 module : 5A => 50 => 0x 00 32 */
+  // Polling the Message From on board charger (Every 500 ms)
+  status STAT;
+
+  // Transmitting Control Message (Every 500ms as well)
+  if(millis()-last_time >= 500) {
+    
+    // This can be problematic as there needs to be a first message to make the OBC not entering COMMUNICATION ERROR
+    // make sure bms both send first then recieve within 500ms. 
+    // Condition 1 Normal BMS message during charge
+    if(STAT.shutdownsig == 1) {
+      bmssent.data[0] = 0x03; // V highbyte 
+      bmssent.data[1] = 0x3E; // V lowbyte 83.0 V fake data
+      bmssent.data[2] = 0x00; // A Highbyte
+      bmssent.data[3] = 0x32; // A Lowbyte 5.0 A fake data
+      bmssent.data[4] = 0x00; // Control Byte 0 charger operate
+      // Not sure if there should be any change to V,A cap limit , just fixed the number by now
+      // Send TTL LOW to Charging shutdown (Passed through builtin Relay driver there, fine.)
+    } else {
+      // Condition 2 Message During Shutdown
+      bmssent.data[0] = 0x00; // V highbyte 
+      bmssent.data[1] = 0x00; // V lowbyte
+      bmssent.data[2] = 0x00; // A Highbyte
+      bmssent.data[3] = 0x00; // A Lowbyte
+      bmssent.data[4] = 0x01; // Control Byte 1 charger shutdown
+      // Turn maximum allowable voltage and current to 0 also as fail safe
+    } mcp2515.sendMessage(MCP2515::TXB1,&bmssent);
+  
+    if (mcp2515.readMessage(&obcreceived) == MCP2515::ERROR_OK) {
+      uint32_t parsedEXTId = obcreceived.can_id & ~CAN_EFF_FLAG;
+      Serial.print("ID: "); Serial.println(parsedEXTId, HEX); 
+      Serial.print("DLC: ");Serial.println(obcreceived.can_dlc, HEX);
+      Serial.print("Data(Bytes): ");
+      for(int i = 0; i < obcreceived.can_dlc; i++) {
+        Serial.print(obcreceived.data[i],HEX); Serial.print(" ");
+      } Serial.println();
+
+      // This block of code execute if detect CAN message from OBC ONLY--
+      if(parsedEXTId== 0x18FF50E5) {
+        // Monitor & Translate current Frame data
+        uint8_t VoutH = obcreceived.data[0];
+        uint8_t VoutL = obcreceived.data[1];
+        uint8_t AoutH = obcreceived.data[2];
+        uint8_t AoutL = obcreceived.data[3];
+        float OBCVolt = mergeHLbyte(VoutH,VoutL)*0.1;
+        float OBCAmp = mergeHLbyte(AoutH,AoutL)*0.1;
+        // OBCVolt = mergeHLbyte(VoutH,VoutL);
+        // OBCAmp = mergeHLbyte(AoutH,AoutL);
+        Serial.print("Voltage from OBC: "); Serial.print(OBCVolt); Serial.println("V");
+        Serial.print("Current from OBC: "); Serial.print(OBCAmp); Serial.println("A");
+        
+        /* Interpret OBC status, and decide on Shutdown command */
+        uint8_t stat =  obcreceived.data[4]; // Status Byte
+        checkstatLSB(&STAT,stat);
+
+        // Shutdown if found any status bit as 1 (abnormal scenario)
+        if(STAT.shutdownsig){
+          digitalWrite(SDCPIN,HIGH);
+          Serial.print("SHUTDOWN: OK ");
+        } else {
+          digitalWrite(SDCPIN,LOW);
+          Serial.print("SHUTDOWN: NOT_OK ");
+          // Shutdown , send the command to OBC to stop , then stop CAN
+        }
+
+        // Intepret Individual bit meaning
+        Serial.print("OBC status: "); 
+        for (short i =0 ; i <8 ; i++)
+          Serial.print(STAT.statbin[i],DEC);
+        Serial.println();
+        
+        switch (STAT.statbin[0]) {
+          case 0:
+            Serial.println("ChargerHW = Normal");
+            break;
+          
+          case 1:
+            Serial.println("ChargerHW = Faulty");
+            break;
+        }
+        switch (STAT.statbin[1]) {
+          case 0:
+            Serial.println("ChargerTemp = Normal");
+            break;
+          
+          case 1:
+            Serial.println("ChargerTemp = Overheat");
+            break;
+        }
+        switch (STAT.statbin[2]) {
+          case 0:
+            Serial.println("ChargerACplug = Normal");
+            break;
+          
+          case 1:
+            Serial.println("ChargerACplug = Reversed");
+            break;
+        }
+        switch (STAT.statbin[3]) {
+          case 0:
+            Serial.println("Charger detects: Vbatt");
+            break;
+          
+          case 1:
+            Serial.println("Charger detects: ZERO Vbatt");
+            break;
+        }
+        switch (STAT.statbin[4]) {
+          case 0:
+            Serial.println("COMMUNICATION STATUS: Normal");
+            break;
+          
+          case 1:
+            Serial.println("COMMUNICATION STATUS: Time out (6s)");
+            break;
+        }
+
+          // How to read status byte
+          // Detect Communication Error
+          // communication timeout on the side of Charger (No command message from BMS is already dealt within OBC control system)
+          // communication timeout of 6s on the side of BMS (6s of not receiving the status update from)
+          // Charging Shutdown Emer button Should Cutout only HV or HV + Signal + LV power , if the
+
+          // uint8_t swverH = canMsgRec.data[5];
+          // uint8_t swverL = canMsgRec.data[6];
+          // uint8_t hwver = canMsgRec.data[7];
+      }
+    }
+    last_time = millis();
+  }
+}
---
+++ util.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <stdio.h>
+#include <util.h>
+// struct status {
+//   uint8_t statbin[8];
+//   bool shutdownsig = 1; // Default should be 1 = OK , 0 = SHUTDOWN
+// };
+// Split and merge High byte low byte of 16 bit unsigned integer
+unsigned char* splitHLbyte(unsigned int num){
+  static uint8_t temp[2]; // initialize
+  temp[0] = (num >> 8) & 255;  // Extract the high byte
+  temp[1] = num & 255;         // Extract the low byte
+  return temp;
+}
+
+// See if we need anything more from original data ,if not use pass by reference
+unsigned int mergeHLbyte(unsigned char Hbyte, unsigned char Lbyte){
+  uint16_t temp = (Hbyte << 8) | Lbyte; // bitshiftLeft by 8 OR with the lower byte, then put to 2 different variable
+  return temp;
+}
+
+// Encode float or uint16 into arrays of uint8, Can we pass by reference ?
+unsigned char* Encode_bytearray(float f) { 
+    // Use memcpy to copy the bytes of the float into the array
+    static uint8_t c[sizeof(f)]; 
+    memcpy(c, &f, sizeof(f));
+    // Copy to address of array , Copy from address of float , size of float: Now, c[0] to c[3] contain the bytes of the float
+    return c; 
+}
+
+// Decode byte arrays into float or uint16 : This one is pass by pointer
+float Decode_bytearray(unsigned char* c) {
+    float f;
+    // Use memcpy to copy the bytes from the array back into the float
+    memcpy(&f, c, sizeof(f));
+    return f;
+}
+
+// Split and Check bit from MSB -> LSB
+unsigned char* checkstatMSB(unsigned char num){
+  static uint8_t arr[8]; // array to hold 8 binary number
+    for (int i = 7; i >= 0; i--){
+        arr[7-i] = (num >> i) & 1;
+        // 1st shift will shift to right by 7 position
+        // Ex 1 => 0b00000001 , then AND with 1 so anything that isn't one at 1st pos will be cut off
+        // 00101010 >> 7 = 00000000 & 00000001 = 0 (point at 1st bit 1 encounter from left . shift to right by 7 pos)
+        // 00101010 >> 6 = 00000000 & 00000001 = 0
+        // 00101010 >> 5 = 00000001 & 00000001 = 1
+        // Doing arr[7-i] because index order is 0->nth not the other way around, in which it conflit a bit with MSB reading
+    }    
+
+  return arr;
+}
+
+// struct status {
+//   uint8_t statbin[8];
+//   bool shutdownsig = 1;
+//   // Default should be 1 = OK , 0 = SHUTDOWN
+// };
+void checkstatLSB(struct status* STAT, unsigned char num){
+  // static uint8_t arr[8]; // array to hold 8 binary number
+  STAT->shutdownsig = 1;
+  for (int i = 0; i < 8; i++){
+    uint8_t bit = num & 1;
+    STAT->statbin[i] = bit;
+    if(bit == 1){
+        STAT->shutdownsig = 0;
+    }
+    num >>= 1; // Right Shift num by 1 pos. before next loop , we AND with 1 again
+
+    // 1st shift will shift to right by 7 position
+        // 1 => 0b00000001 , then AND with 1 so anything that isn't one at 1st pos will be cut off
+        // Ex. 42 = 0b00101010
+        // 00101010 >> 0 = 00101010 & 00000001 = 0 (point at 1st bit 1 encounter from left . shift to right by 0 pos)
+        // 00101010 >> 1 = 00010101 & 00000001 = 1
+        // 00101010 >> 2 = 00001010 & 00000001 = 0
+    // Check for bit 1 for immediate shutdown
+  } 
+}
+
+status checkstatLSB(struct status STAT, unsigned char num){
+  // static uint8_t arr[8]; // array to hold 8 binary number
+  for (int i = 0; i < 8; i++){
+    STAT.statbin[i] = (num >> i) & 1;
+    // 1st shift will shift to right by 7 position
+        // 1 => 0b00000001 , then AND with 1 so anything that isn't one at 1st pos will be cut off
+        // Ex. 42 = 0b00101010
+        // 00101010 >> 0 = 00101010 & 00000001 = 0 (point at 1st bit 1 encounter from left . shift to right by 0 pos)
+        // 00101010 >> 1 = 00010101 & 00000001 = 1
+        // 00101010 >> 2 = 00001010 & 00000001 = 0
+    // Check for bit 1 for immediate shutdown
+    if(STAT.statbin[i]==1)
+      STAT.shutdownsig = 0;
+  }
+  return STAT; 
+}
+
+// Split and Check bit from LSB -> MSB
+unsigned char *checkstatLSB(unsigned char num){
+  static uint8_t arr[8]; // array to hold 8 binary number
+  for (int i = 0; i < 8; i++){
+    arr[i] = (num >> i) & 1;
+    // 1st shift will shift to right by 7 position
+        // 1 => 0b00000001 , then AND with 1 so anything that isn't one at 1st pos will be cut off
+        // Ex. 42 = 0b00101010
+        // 00101010 >> 0 = 00101010 & 00000001 = 0 (point at 1st bit 1 encounter from left . shift to right by 0 pos)
+        // 00101010 >> 1 = 00010101 & 00000001 = 1
+        // 00101010 >> 2 = 00001010 & 00000001 = 0
+    // Check for bit 1 for immediate shutdown
+  } return arr; 
+}
+
+// Test & Debug
+
+// int main() {
+//   uint8_t num = 8;  // Example decimal number (42)
+//   // unsigned char* x;
+//   // // Read & Store from MSB
+//   // x = checkstatMSB(num);
+//   // for (int i=0; i < 8 ; i++){
+//   //   printf("%d", x[i]);
+//   // }
+//   // putc('\n', stdout);
+  
+//   // Read & Store from LSB
+//   // x = checkstatLSB(num);
+//   // for (int i=0; i < 8 ; i++){
+//   //   printf("%d", x[i]);
+//   // }
+//   // putc('\n', stdout);
+
+//   // Read & Store from lSB in Strcut type
+//   status STAT;
+//   checkstatLSB(&STAT,num);
+//   for (int i=0; i < 8 ; i++){
+//     printf("%d", STAT.statbin[i]);
+//   }
+//   putc('\n', stdout);
+//   printf("%d", STAT.shutdownsig);
+//   putc('\n', stdout);
+
+
+//   // // Split HL byte
+//   // uint16_t a = 1279;
+//   // x = splitHLbyte(a);
+//   // printf("%d", x[0]);
+//   // putc('\n', stdout);
+//   // printf("%d", x[1]);
+//   // putc('\n', stdout);
+
+//   // // Merge HL byte
+//   // uint16_t b = mergeHLbyte(x[0],x[1]);
+//   // // uint16_t b = mergeHLbyte(0b0100,0b11111111);
+//   // printf("%d", b);
+//   // putc('\n', stdout);
+
+//   return 0;
+// }
